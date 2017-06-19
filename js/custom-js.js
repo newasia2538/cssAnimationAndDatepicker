2d5f9a36c5c3f661ba341f70c0d5e206dcfda784
Maii <mr.kyoya@gmail.com>
2017-06-19 12:46:22 +0700

    change year in datepicker to Thai year

--- js/custom-js.js
+++ js/custom-js.js
@@ -6,3 +6,8 @@ $('.button-collapse').sideNav({
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
